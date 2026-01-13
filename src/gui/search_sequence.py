0b8af116995b3eeac45bb04fe3d282db40a1944f
sebastien-relier <75561328+sebastien-relier@users.noreply.github.com>
2026-01-13 16:39:34 -0500

    Create QLineEdit to Search Sequence Within Trace

---
+++ src/gui/search_sequence.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 27 12:42:39 2025
+
+@author: sebastien
+"""
+
+
+
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
+import re
+
+class SearchSequence(QLineEdit):
+    
+    ''' Search the sequence from Trace '''
+    
+    def __init__(self, main):
+    
+        super().__init__()
+        
+        self.main = main
+        
+        self.setPlaceholderText("Search sequence")
+        self._create_validator()
+        
+        self.textChanged.connect(self.onChange)
+    
+    def _create_validator(self):
+        
+        # -- allow the input of G,A,T,C only -- #
+        
+        regex = QRegExp("[GATCNgatcn]+")
+        validator = QRegExpValidator(regex)
+        self.setValidator(validator)
+
+    def onChange(self):
+        
+        # -- update the plot to be at the sequence -- #
+        self.setText(self.text().upper())
+        self.main.plot.subseq = self.text().replace("N",".")
+        self.main.plot.go_to_subsequence()
+        self.control_existence()
+            
+    def control_existence(self):
+        
+        pos = re.findall(self.main.plot.subseq, self.main.plot.seq)
+        
+        if len(pos) > 0:
+                # -- change color of the text based on the existence of the sequence -- #    
+            self.setStyleSheet("color:black")
+        else:
+            self.setStyleSheet("color:red")
